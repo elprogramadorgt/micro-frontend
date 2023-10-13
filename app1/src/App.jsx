26b57a2ad95e85f1603403978ef33e1e83a0b12f
elprogramadorgt8@gmail.com <elprogramadorgt8@gmail.com>
2023-10-12 22:19:02 -0500

    -- remove unsued component

--- app1/src/App.jsx
+++ app1/src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-import Edu from "./Edu"
+
 import Iluminasaurio from './Iluminasaurio'
 
 function App() {
